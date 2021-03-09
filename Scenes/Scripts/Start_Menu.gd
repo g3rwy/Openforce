211c51f669a7760c34524389af509fcb3d3964fb
g3rwy <gerwy@mailbox.org>
2021-03-09 22:27:18 +0100

    AAAA, how to make buttons not affected by shader

--- Scenes/Scripts/Start_Menu.gd
+++ Scenes/Scripts/Start_Menu.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var star = preload("res://Assets/Star.tscn")
-onready var stars_root = $Background/Stars
+onready var stars_root = $Screen/Background/Stars
 
 func _ready():
 	#yield(get_tree().create_timer(4), 'timeout')
@@ -13,3 +13,11 @@ func _process(delta):
 	pass
 
 #TODO Make UI under Stars_Shader, with that the Chromatic abberation should affect it
+
+
+func _on_quit():
+	get_tree().quit()
+
+
+func _on_play():
+	$"Screen/Menu/test".visible = true
